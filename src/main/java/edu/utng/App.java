d6f5d1621a437569409184b2cd79f7c226eb9a34
tuxmex <crackiman@gmail.com>
2022-05-09 16:58:15 -0500

    Cambio del programa.

--- src/main/java/edu/utng/App.java
+++ src/main/java/edu/utng/App.java
@@ -1,5 +1,7 @@
 package edu.utng;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Scanner s = new Scanner(System.in);
+        System.out.println("Teclea tu nombre:");
+        String name = s.nextLine();
+        System.out.println("Name: "+name);
     }
 }
